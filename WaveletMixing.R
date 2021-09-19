67b95218ea477db67c3c12115a51db89b1643225
oorejola <oorejola@gmail.com>
2021-09-19 10:15:20 -0500

    Added MC Power Test and Simple plot for two hurst parameters

--- WaveletMixing.R
+++ WaveletMixing.R
@@ -13,12 +13,14 @@ library(diptest)
 
 H0 = 0.25
 H1 = 0.75
-level = 9
-scale = 4
+level = 14
+scale = 8
 mixingrate = 0.5
 
+N = 2^level #Path size
+n_j = N/(2^scale)#effective sample size
 
-alpha = 0.75 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
+alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
 p = floor(alpha*n_j) #dimension i.e. number of fBM we sample
 
 
@@ -46,12 +48,59 @@ mixed_wavelet_sampe_covariance <- function(level,scale,p,H0,H1,mixingrate){
 
 E_MIXED <- mixed_wavelet_sampe_covariance(level,scale,p,H0,H1,mixingrate)
 
+monte_carlo_power_two_hurst <- function(runs,significance,level,scale,p,mixingrate){
+  count = 0
+  for(i in 1:runs){
+    E_MIXED <- mixed_wavelet_sampe_covariance(level,scale,p,H0,H1,mixingrate)
+    pvalue<-dip.test(E_MIXED)$p.value
+    if( pvalue<= significance){
+      count = count + 1
+    }
+  }
+  return(count/runs)
+}
+
+
+#-----------------------------------------------------
+
+H0 = 0.25
+H1 = 0.75
+mixingrate = 0.5
+
+for(i in 1:5){
+  level = 9+i
+  scale = 1+i
+  N = 2^level #Path size
+  n_j = N/(2^scale)#effective sample size
+  alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
+  p = floor(alpha*n_j) #
+  power <-monte_carlo_power_two_hurst(1000,0.05,level,scale,p,0.5)
+  print(paste(scale,level,p,power))
+}
+
+for(i in 1:5){
+  level = 9+i
+  scale = 2+i
+  N = 2^level #Path size
+  n_j = N/(2^scale)#effective sample size
+  alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
+  p = floor(alpha*n_j) #
+  power <-monte_carlo_power_two_hurst(1000,0.05,level,scale,p,0.5)
+  print(paste(scale,level,p,power))
+}
+
+
+plot(c(10,11,12,13,14),c(0,0,0.776,0.988,0.997),type = "o")
+
+
+
+
 pvalue<-dip.test(E_MIXED)$p.value
 
 alpha = 0.5
 significance = 0.05 #Rejection  region
 
-for(i in 9:14){
+for(i in 9:12){
   for(j in 4:(i-1)){
     p = floor( alpha * 2^(i-j))
     E_MIXED <- mixed_wavelet_sampe_covariance(i,j,p,H0,H1,mixingrate)

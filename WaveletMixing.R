4fc12fe304e62d1aad61da0eb1404184068b0a1d
Oliver Orejola <oorejola@gmail.com>
2021-09-21 21:41:14 -0500

    Overhaled general n hurst parameter functionality. Added coordinate matrix.

--- WaveletMixing.R
+++ WaveletMixing.R
@@ -1,17 +1,67 @@
 # Oliver Orejola
 # Created: 7/2/21
-# Updated: 9/16/21
+# Updated: 9/21/21
 # Wavelet ESD for Mixed Hurst Parameters
-
-
-
-#########################[ MIXED ]#############################
-#######################[ Run First ]###########################
+#------------------------- [ Libraries ]----------------------------
 library(somebm)
 library(wavelets)
 library(diptest)
 library(ggplot2)
 
+
+#------------------------- [ Functions ]----------------------------
+
+coordinate_matrix <- function(n){
+  mat <- c()
+  for(i in 1:n){
+    x <- rnorm(n)
+    col <- x/sqrt(sum(x^2)) 
+    mat <- append(mat,col)
+  }
+  return(matrix(mat, n, n,byrow = FALSE))
+}
+
+wavelet_sampe_covariance<- function(level,scale,p,Hursts, mixed = FALSE){
+  N = 2^level
+  n_j = N/(2^scale) #effective sample size
+  wavelet_list <- c() 
+  for(i in 1:p){
+    H <- sample(Hursts,1)
+    #MIXED<-  N^H*fbm(hurst = H, n = N) 
+    fractional_brownian <-  N^H*fbm(hurst = H, n = N) 
+    wavelet_transform <- dwt(fractional_brownian, filter = "d4", n.levels = level, boundary = "reflection")
+    wavelet_list <- append(wavelet_list,unlist(wavelet_transform@W[scale]))
+  }
+  wavelet_mat<- matrix(wavelet_list, n_j, p,byrow = FALSE)
+  
+  if(mixed == TRUE){ 
+    P <- coordinate_matrix(p)
+    mixed_wavlet_mat <- wavelet_mat %*% t(P)
+    mixed_wavlet_product <- crossprod(mixed_wavlet_mat)
+    eigenvalues <- eigen(1/n_j*mixed_wavlet_product)$values
+  }
+  else{
+    wavelet_product <-  crossprod(wavelet_mat)
+    eigenvalues <- eigen(1/n_j*wavelet_product)$values
+  }
+  return(eigenvalues)
+}
+
+monte_carlo_probability_of_rejection <- function(runs,significance,level,scale,p,hursts){
+  count = 0
+  for(i in 1:runs){
+    E_MIXED <- log(wavelet_sampe_covariance(level,scale,p,hursts))
+    pvalue<-dip.test(E_MIXED)$p.value
+    if( pvalue<= significance){
+      count = count + 1
+    }
+  }
+  proportion_of_rejections = count/runs
+  return(proportion_of_rejections)
+}
+
+
+#-----------------------------------------------------
 H0 = 0.25
 H1 = 0.75
 level = 14
@@ -24,74 +74,6 @@ n_j = N/(2^scale)#effective sample size
 alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
 p = floor(alpha*n_j) #dimension i.e. number of fBM we sample
 
-
-mixed_wavelet_sampe_covariance <- function(level,scale,p,H0,H1,mixingrate){
-  N = 2^level
-  n_j = N/(2^scale) #effective sample size
-  X_MIXED <- c()
-  for(i in 1:p){
-    MIXED<- c()
-    if( rbinom(1,1,mixingrate) == 1){
-      fbmSIM_1 <- N^H1*fbm(hurst = H1, n = N)
-      MIXED<- fbmSIM_1
-    }
-    else{
-      fbmSIM_0 <- N^H0*fbm(hurst = H0, n = N)
-      MIXED<- fbmSIM_0
-    }
-    wave_MIXED <- dwt(MIXED, filter = "d4", n.levels = level, boundary = "reflection")
-    X_MIXED<- append(X_MIXED,unlist(wave_MIXED@W[scale]))
-  }
-  Xmat_MIXED <- matrix( X_MIXED, n_j, p,byrow = FALSE)  
-  W_MIXED <-  crossprod(Xmat_MIXED)
-  return(log(eigen(1/n_j*W_MIXED)$values))
-}
-
-
-
-N_mixed_wavelet_sampe_covariance <- function(level,scale,p,Hursts){
-  N = 2^level
-  n_j = N/(2^scale) #effective sample size
-  X_MIXED <- c()
-  for(i in 1:p){
-    MIXED<- c()
-    H <- sample(Hursts,1)
-    MIXED<-  N^H*fbm(hurst = H, n = N)
-    wave_MIXED <- dwt(MIXED, filter = "d4", n.levels = level, boundary = "reflection")
-    X_MIXED<- append(X_MIXED,unlist(wave_MIXED@W[scale]))
-  }
-  Xmat_MIXED <- matrix( X_MIXED, n_j, p,byrow = FALSE)  
-  W_MIXED <-  crossprod(Xmat_MIXED)
-  return(log(eigen(1/n_j*W_MIXED)$values))
-}
-
-
-monte_carlo_power_two_hurst <- function(runs,significance,level,scale,p,mixingrate){
-  count = 0
-  for(i in 1:runs){
-    E_MIXED <- mixed_wavelet_sampe_covariance(level,scale,p,H0,H1,mixingrate)
-    pvalue<-dip.test(E_MIXED)$p.value
-    if( pvalue<= significance){
-      count = count + 1
-    }
-  }
-  return(count/runs)
-}
-
-monte_carlo_power_N_hursts <- function(runs,significance,level,scale,p,hursts){
-  count = 0
-  for(i in 1:runs){
-    E_MIXED <- N_mixed_wavelet_sampe_covariance(level,scale,p,hursts)
-    pvalue<-dip.test(E_MIXED)$p.value
-    if( pvalue<= significance){
-      count = count + 1
-    }
-  }
-  return(count/runs)
-}
-
-#-----------------------------------------------------
-
 H0 = 0.25
 H1 = 0.75
 mixingrate = 0.5
@@ -103,20 +85,10 @@ for(i in 1:5){
   n_j = N/(2^scale)#effective sample size
   alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
   p = floor(alpha*n_j) #
-  power <-monte_carlo_power_two_hurst(1000,0.05,level,scale,p,0.5)
+  power <-monte_carlo_probability_of_rejection(1000,0.9,level,scale,p,c(0.25,0.75))
   print(paste(scale,level,p,power))
 }
 
-for(i in 1:5){
-  level = 9+i
-  scale = 2+i
-  N = 2^level #Path size
-  n_j = N/(2^scale)#effective sample size
-  alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
-  p = floor(alpha*n_j) #
-  power <-monte_carlo_power_two_hurst(1000,0.05,level,scale,p,0.5)
-  print(paste(scale,level,p,power))
-}
 
 
 plot(c(10,11,12,13,14),c(0,0,0.776,0.988,0.997),type = "o")
@@ -130,7 +102,7 @@ significance = 0.05 #Rejection  region
 for(i in 9:16){
   for(j in 4:(i-1)){
     p = floor( alpha * 2^(i-j))
-    #E_MIXED <- mixed_wavelet_sampe_covariance(i,j,p,H0,H1,mixingrate)
+    #E_MIXED <- log(wavelet_sampe_covariance(i,j,p,H0,H1,mixingrate))
     #pvalue<-dip.test(E_MIXED)$p.value
     pvalue = 0
     if( pvalue<= significance){
@@ -146,7 +118,7 @@ for(i in 9:16){
 par(mfrow = c(1,1))
 
 
-Etest <- N_mixed_wavelet_sampe_covariance(16,8,50,c(0.2,0.8))
+Etest <- log(wavelet_sampe_covariance(16,8,50,c(0.2,0.8)))
 hist(Etest)
 
 
@@ -155,21 +127,12 @@ M <- max(Etest)
 
 hist(Etest,probability=TRUE
      ,breaks = seq(m-1,M+1, by = 0.25)
-     ,xlim = c(m,M+5)
+     ,xlim = c(m,M)
 )
 
-monte_carlo_power_N_hursts(10,0.05,15,7,50,c(0.2,0.8,0.4,0.6))
+monte_carlo_probability_of_rejection(10,0.05,15,7,50,c(0.2,0.8,0.4,0.6))
 
-E_MIXED <- mixed_wavelet_sampe_covariance(level,scale,p,H0,H1,mixingrate)
-m <- min(E_MIXED)
-M <- max(E_MIXED)
 
-hist(E_MIXED,probability=TRUE
-     ,breaks = seq(m-1,M+1, by = 0.25)
-     ,xlim = c(m,M+5)
-     ,xlab = "Eigenvalues"
-     ,main = paste("H_0=",H0,", H_1=",H1, ", n_j=",n_j,", p/n_j=",alpha)
-)
 
 #-------------------------------------- dimension = 32
 #----------------------- 2 hurst parameters 
@@ -179,7 +142,7 @@ for(level in 10:13){
   n_j = N/(2^scale)#effective sample size
   alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
   p = floor(alpha*n_j) #
-  power <-monte_carlo_power_N_hursts(100,0.90,level,scale,p,c(0.27,0.75))
+  power <-monte_carlo_probability_of_rejection(100,0.90,level,scale,p,c(0.27,0.75))
   print(paste(scale,level,p,power))
 }
 #---- FIXED SCALE
@@ -188,7 +151,7 @@ for(level in 10:13){
 for(scale in 5:8){
   level = scale*2
   p = 2^(level- 1-scale) #given alpha = 1/2
-  power <-monte_carlo_power_N_hursts(1000,0.90,level,scale,p,c(0.5))
+  power <-monte_carlo_probability_of_rejection(1000,0.90,level,scale,p,c(0.5))
   print(paste(scale,level,p,power))
 }
 
@@ -196,14 +159,14 @@ for(scale in 5:8){
 for(scale in 5:8){
   level = scale*2
   p = 2^(level- 1-scale) #given alpha = 1/2
-  power <-monte_carlo_power_N_hursts(10,0.90,level,scale,p,c(0.27,0.75))
+  power <-monte_carlo_probability_of_rejection(10,0.90,level,scale,p,c(0.27,0.75))
   print(paste(scale,level,p,power))
 }
 #---- Triple Limit 4 Hurst
 for(scale in 5:8){
   level = scale*2
   p = 2^(level- 1-scale) #given alpha = 1/2
-  power <-monte_carlo_power_N_hursts(10,0.90,level,scale,p,c(0.2,0.4,0.6,0.8))
+  power <-monte_carlo_probability_of_rejection(10,0.90,level,scale,p,c(0.2,0.4,0.6,0.8))
   print(paste(scale,level,p,power))
 }
 
@@ -215,7 +178,7 @@ for(level in 10:13){
   n_j = N/(2^scale)#effective sample size
   alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
   p = floor(alpha*n_j) #
-  power <-monte_carlo_power_N_hursts(100,0.90,level,scale,p,c(0.2,0.4,0.6,0.8))
+  power <-monte_carlo_probability_of_rejection(100,0.90,level,scale,p,c(0.2,0.4,0.6,0.8))
   print(paste(scale,level,p,power))
 }
 
@@ -226,7 +189,7 @@ for(level in 10:13){
   n_j = N/(2^scale)#effective sample size
   alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
   p = floor(alpha*n_j) #
-  power <-monte_carlo_power_N_hursts(100,0.95,level,scale,p,c(0.5))
+  power <-monte_carlo_probability_of_rejection(100,0.95,level,scale,p,c(0.5))
   print(paste(scale,level,p,power))
 }
 
@@ -238,7 +201,7 @@ for(level in 10:16){
   n_j = N/(2^scale)#effective sample size
   alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
   p = floor(alpha*n_j) #
-  power <-monte_carlo_power_N_hursts(1000,0.05,level,scale,p,c(0.2,0.5,0.8))
+  power <-monte_carlo_probability_of_rejection(1000,0.05,level,scale,p,c(0.2,0.5,0.8))
   print(paste(scale,level,p,power))
 }
 
@@ -249,13 +212,13 @@ for(level in 10:16){
   n_j = N/(2^scale)#effective sample size
   alpha = 0.5 # effective sample size and dimension ratio, must be greater than 1 for rank sufficiency 
   p = floor(alpha*n_j) #
-  power <-monte_carlo_power_N_hursts(100,0.05,level,scale,p,c(0.1,0.25,0.40,0.55,0.70,0.85))
+  power <-monte_carlo_probability_of_rejection(100,0.05,level,scale,p,c(0.1,0.25,0.40,0.55,0.70,0.85))
   print(paste(scale,level,p,power))
 }
 
 
 par(mfrow = c(1,1))
-E_MIXED <- N_mixed_wavelet_sampe_covariance(15,6,128,c(0.1,0.25,0.40,0.55,0.70,0.85))
+E_MIXED <- log(wavelet_sampe_covariance(15,6,128,c(0.1,0.25,0.40,0.55,0.70,0.85)))
 m <- min(E_MIXED)
 M <- max(E_MIXED)
 N = 2^15 #Path size
@@ -269,7 +232,7 @@ hist(E_MIXED,probability=TRUE
 )
 
 
-#--------- Plots -----------------------
+#---------[ Plots ] (Fixed dimension)-----------------------
 par(mfrow = c(2,2))
 xaxis<- c(10:16)
 #--------- 1 hurst ---------------------
@@ -302,16 +265,8 @@ plot(xaxis,power_4_hurst,type="o"
 
 
 
-#--------- 6 hurst ---------------------
 
-power_6_hurst <- c(0,0,0,0,0.052,0.104,0.2)
-plot(xaxis,power_6_hurst,type="o"
-     ,xlab = "level",ylim=c(0,1)
-     ,ylab = "Proportion of Rejections"
-     ,main = "6 Hurst Parameters")
-
-
-E<- N_mixed_wavelet_sampe_covariance(16,8,64,c(0.5))/(j*log(2))
+E<- log(wavelet_sampe_covariance(16,8,64,c(0.5)))/(j*log(2))
 m=min(E)
 M=max(E)
 hist(E,probability=TRUE
@@ -320,3 +275,38 @@ hist(E,probability=TRUE
      ,xlab = "Eigenvalues"
 )
 mean(E)
+
+#-------- Investigate Scales Triple Limit---------------------
+for(scale in 5:8){
+  level = scale*2
+  p = 2^(level- 1-scale)
+  E <- log(wavelet_sampe_covariance(level,scale,p,c(0.5)))/(scale*log(2))
+  mean <- mean(E)
+  print(paste(scale,level,as.integer(p), mean))
+}
+
+
+for(scale in 5:8){
+  level = scale*2
+  p = 2^(level- 1-scale)
+  E <- log(wavelet_sampe_covariance(level,scale,p,c(0.75)))/(scale*log(2))
+  mean <- mean(E)
+  print(paste(scale,level,as.integer(p), mean))
+}
+
+for(scale in 5:13){
+  level = 18
+  p = 32
+  E <- log(wavelet_sampe_covariance(level,scale,p,c(0.75)))/(scale*log(2))
+  mean <- mean(E)
+  print(paste(scale,level,as.integer(p), mean))
+}
+for(scale in 5:13){
+  level = 18
+  p = 32
+  E <- log(wavelet_sampe_covariance(level,scale,p,c(0.75)))/(scale*log(2))
+  mean <- mean(E)
+  print(paste(scale,level,as.integer(p), mean))
+}
+
+
